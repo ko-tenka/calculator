752144ce08faa1658ad5a0eec339a61f4976d619
ko-tenka <kotelnikova123.123@gmail.com>
2025-07-13 18:27:20 +0300

    release style var

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,18 @@ import FenceCalculator from './components/FenceCalculator.vue'
 </template>
 
 <style>
- #app {
+#app {
   font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin: 0;
-  /* padding: 20px;  */
   min-height: 100vh;
-  /* background: linear-gradient(135deg, #667eea 0%, #764ba2 100%); */
-  background: linear-gradient(135deg, #8da0f8 0%, #9871c0 100%);
+  background: var( --app-bg);
   display: flex;
   justify-content: center;
   align-items: center;
+  color: var(--app-color);
 } 
 
 body {
